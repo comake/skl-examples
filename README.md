f94181e353c5073a656e91fd50ce61fd4640ed67
Adler Faulkner <aif28@cornell.edu>
2023-01-15 21:07:46 -1000

    standard sdk examples

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# skl-examples
-Examples for using Standard Knowledge Language via Standard SDK JS and SKQL JS Engine
+# Welcome to SKL Examples!
+
+This repository contains examples of using Standard Knowledge Language via Standard SDK JS and SKQL JS Engine.
+
+## Featured Examples
+
+- Standard SDK
+
+  - [Send requests to an API using SKL](https://github.com/comake/skl-examples/blob/main/standard-sdk-js/ticketmaster-openapi-spec)
+
+  - [Send requests to an API using OpenAPI specifications](https://github.com/comake/skl-examples/blob/main/standard-sdk-js/ticketmaster-using-skl)
+
+- SKQL Engine - examples coming soon!
---
+++ standard-sdk-js/ticketmaster-using-skl/src/Util.ts
@@ -0,0 +1,30 @@
+import { promises as fs } from 'fs';
+import type { Entity } from '@comake/standard-sdk-js';
+import jsonld from 'jsonld';
+
+export async function frameAndCombineSchemas(
+  filePaths: string[],
+  env: Record<string, string> = {},
+): Promise<Entity[]> {
+  const schemas = await Promise.all(
+    filePaths.map(async(filePath: string): Promise<jsonld.NodeObject[]> => {
+      let schema = await fs.readFile(filePath, { encoding: 'utf8' });
+      Object.keys(env).forEach((envVar: string): void => {
+        schema = schema.replace(`ENV_${envVar}`, env[envVar]);
+      });
+      return await jsonld.expand(JSON.parse(schema));
+    }),
+  );
+  const expandedSchema = schemas.flat();
+  const nonBlankNodes = expandedSchema
+    .map((schema): string | undefined => schema['@id'] as string | undefined)
+    .filter((id): boolean => id !== undefined && !id.startsWith('_:'));
+  const framedSchema = await jsonld.frame(
+    expandedSchema,
+    {
+      '@context': {},
+      '@id': nonBlankNodes as any,
+    },
+  );
+  return framedSchema['@graph'] as Entity[];
+}
