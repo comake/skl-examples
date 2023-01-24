aa8dd24443a0027871f6a04134fe4a8c598caa4b
Adler Faulkner <aif28@cornell.edu>
2023-01-23 19:03:04 -1000

    add comments

--- unified-events-api/src/UsingStandardSdkWithOpenApiSpecs.ts
+++ unified-events-api/src/UsingStandardSdkWithOpenApiSpecs.ts
@@ -8,6 +8,7 @@ import stubhubOpenapi from './openapi-specs/StubhubOpenapi';
 import ticketmasterOpenapi from './openapi-specs/TicketmasterOpenapi';
 
 (async function(): Promise<void> {
+  // Built our Standard SDK
   const standardSDK = StandardSDK.build({
     apiSpecs: {
       ticketmaster: { type: 'openapi', value: ticketmasterOpenapi },
@@ -16,18 +17,21 @@ import ticketmasterOpenapi from './openapi-specs/TicketmasterOpenapi';
     },
   });
 
+  // Get events from Ticketmaster using the `SearchEvents` operation in the OpenAPI spec
   const ticketmasterResponse = await standardSDK.ticketmaster.SearchEvents(
     { city: 'New York' },
     { apiKey: process.env.TICKETMASTER_APIKEY }
   );
   console.log(JSON.stringify(ticketmasterResponse.data));
 
+  // Get events from Stubhub using the `Events_SearchEvents` operation in the OpenAPI spec
   const stubhubResponse = await standardSDK.stubhub.Events_SearchEvents(
     { q: 'New York' },
     { accessToken: process.env.STUBHUB_ACCESS_TOKEN }
   );
   console.log(JSON.stringify(stubhubResponse.data));
 
+  // Get events from SeatGeek using the `searchEvents` operation in the OpenAPI spec
   const seatgeekResponse = await standardSDK.seatgeek.searchEvents(
     { q: 'New York' },
     { apiKey: process.env.SEATGEEK_APIKEY }
--- unified-events-api/src/UsingStandardSdkWithSkl.ts
+++ unified-events-api/src/UsingStandardSdkWithSkl.ts
@@ -18,24 +18,29 @@ const env = {
 };
 
 (async function(): Promise<void> {
+  // Get contents of schema files, combine and frame them, replace stubbed env vars with real values
   const schemas = await combineSchemas(schemaFiles, env);
 
+  // Build our Standard SDK
   const standardSdk = StandardSDK.build({
     skqlOptions: { type: 'memory', schemas },
   });
 
+  // Get events from Ticketmaster using the `getEvents` Verb. Response holds a list of schema.org/Event entities
   const ticketmasterResponse = await standardSdk.skql.verb.getEvents({
     account: 'https://example.com/data/TicketmasterAccount',
     city: 'New York',
   });
   console.log(JSON.stringify(ticketmasterResponse['https://skl.standard.storage/records']));
 
+  // Get events from Stubhub using the `getEvents` Verb. Response holds a list of schema.org/Event entities
   const stubhubResponse = await standardSdk.skql.verb.getEvents({
     account: 'https://example.com/data/StubhubAccount',
     city: 'New York',
   });
   console.log(JSON.stringify(stubhubResponse['https://skl.standard.storage/records']));
 
+  // Get events from SeatGeek using the `getEvents` Verb. Response holds a list of schema.org/Event entities
   const seatgeekResponse = await standardSdk.skql.verb.getEvents({
     account: 'https://example.com/data/SeatgeekAccount',
     city: 'New York',
